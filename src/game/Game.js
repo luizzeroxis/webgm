882c2c736bc880d1b8fafb4039a4683148d9f306
Luiz <eldorado.luiz@gmail.com>
2022-07-07 21:58:22 -0300

    No longer using KeyboardEvent.which.

--- src/game/Game.js
+++ src/game/Game.js
@@ -12,6 +12,177 @@ import GML from "./GML.js";
 import Instance from "./Instance.js";
 
 export default class Game {
+	static codeToWhichMap = {
+		"": -1,
+		"Unidentified": -1,
+
+		"Backspace": 8,
+		"Tab": 9,
+		"Enter": 13,
+		"NumpadEnter": 13,
+		"ShiftLeft": 16,
+		"ShiftRight": 16,
+		"ControlLeft": 17,
+		"ControlRight": 17,
+		"AltLeft": 18,
+		"AltRight": 18,
+		"Pause": 19,
+		"CapsLock": 20,
+		"Escape": 27,
+		"Space": 32,
+		"PageUp": 33,
+		"PageDown": 34,
+		"End": 35,
+		"Home": 36,
+		"ArrowLeft": 37,
+		"ArrowUp": 38,
+		"ArrowRight": 39,
+		"ArrowDown": 40,
+		"PrintScreen": 44,
+		"Insert": 45,
+		"Delete": 46,
+		"Digit0": 48,
+		"Digit1": 49,
+		"Digit2": 50,
+		"Digit3": 51,
+		"Digit4": 52,
+		"Digit5": 53,
+		"Digit6": 54,
+		"Digit7": 55,
+		"Digit8": 56,
+		"Digit9": 57,
+		"KeyA": 65,
+		"KeyB": 66,
+		"KeyC": 67,
+		"KeyD": 68,
+		"KeyE": 69,
+		"KeyF": 70,
+		"KeyG": 71,
+		"KeyH": 72,
+		"KeyI": 73,
+		"KeyJ": 74,
+		"KeyK": 75,
+		"KeyL": 76,
+		"KeyM": 77,
+		"KeyN": 78,
+		"KeyO": 79,
+		"KeyP": 80,
+		"KeyQ": 81,
+		"KeyR": 82,
+		"KeyS": 83,
+		"KeyT": 84,
+		"KeyU": 85,
+		"KeyV": 86,
+		"KeyW": 87,
+		"KeyX": 88,
+		"KeyY": 89,
+		"KeyZ": 90,
+		"MetaLeft": 91,
+		"OSLeft": 91,
+		"MetaRight": 92,
+		"OSRight": 92,
+		"ContextMenu": 93,
+		"Numpad0": 96,
+		"Numpad1": 97,
+		"Numpad2": 98,
+		"Numpad3": 99,
+		"Numpad4": 100,
+		"Numpad5": 101,
+		"Numpad6": 102,
+		"Numpad7": 103,
+		"Numpad8": 104,
+		"Numpad9": 105,
+		"NumpadMultiply": 106,
+		"NumpadAdd": 107,
+		"NumpadSubtract": 109,
+		"NumpadDecimal": 110,
+		"NumpadDivide": 111,
+		"F1": 112,
+		"F2": 113,
+		"F3": 114,
+		"F4": 115,
+		"F5": 116,
+		"F6": 117,
+		"F7": 118,
+		"F8": 119,
+		"F9": 120,
+		"F10": 121,
+		"F11": 122,
+		"F12": 123,
+		"NumLock": 144,
+		"ScrollLock": 145,
+		// vk_lshift: 160,
+		// vk_rshift: 161,
+		// vk_lcontrol: 162,
+		// vk_rcontrol: 163,
+		// vk_lalt: 164,
+		// vk_ralt: 165,
+		"Semicolon": 186,
+		"Equal": 187,
+		"Comma": 188,
+		"Minus": 189,
+		"Period": 190,
+		"Slash": 191,
+		"Backquote": 192,
+		"NumpadComma": 194,
+		"BracketLeft": 219,
+		"Backslash": 220,
+		"BracketRight": 221,
+		"Quote": 222,
+		"IntlBackslash": 226,
+
+		"AudioVolumeDown": -1,
+		"AudioVolumeMute": -1,
+		"AudioVolumeUp": -1,
+		"BrowserBack": -1,
+		"BrowserFavorites": -1,
+		"BrowserForward": -1,
+		"BrowserHome": -1,
+		"BrowserRefresh": -1,
+		"BrowserSearch": -1,
+		"BrowserStop": -1,
+		"Convert": -1,
+		"Copy": -1,
+		"Cut": -1,
+		"Eject": -1,
+		"F13": -1,
+		"F14": -1,
+		"F15": -1,
+		"F16": -1,
+		"F17": -1,
+		"F18": -1,
+		"F19": -1,
+		"F20": -1,
+		"F21": -1,
+		"F22": -1,
+		"F23": -1,
+		"F24": -1,
+		"IntlRo": -1,
+		"IntlYen": -1,
+		"KanaMode": -1,
+		"Lang1": -1,
+		"Lang2": -1,
+		"Lang3": -1,
+		"Lang4": -1,
+		"LaunchApp1": -1,
+		"LaunchApp2": -1,
+		"LaunchMail": -1,
+		"MediaPlayPause": -1,
+		"MediaSelect": -1,
+		"MediaStop": -1,
+		"MediaTrackNext": -1,
+		"MediaTrackPrevious": -1,
+		"NonConvert": -1,
+		"NumpadEqual": -1,
+		"Paste": -1,
+		"Power": -1,
+		"Sleep": -1,
+		"Undo": -1,
+		"VolumeDown": -1,
+		"VolumeUp": -1,
+		"WakeUp": -1,
+	};
+
 	constructor(project, canvas, input) {
 		this.project = new Project(project);
 		this.canvas = canvas;
@@ -232,11 +403,14 @@ export default class Game {
 
 		this.keyDownHandler = (e) => {
 			e.preventDefault();
-			this.key[e.which] = true;
-			this.keyPressed[e.which] = true;
 
-			this.currentKey = e.which;
-			this.lastKey = e.which;
+			const key = Game.codeToWhichMap[e.code];
+
+			this.key[key] = true;
+			this.keyPressed[key] = true;
+
+			this.currentKey = key;
+			this.lastKey = key;
 
 			if (e.key.length == 1) {
 				this.lastKeyChar = e.key;
@@ -250,8 +424,11 @@ export default class Game {
 
 		this.keyUpHandler = (e) => {
 			e.preventDefault();
-			this.key[e.which] = false;
-			this.keyReleased[e.which] = true;
+
+			const key = Game.codeToWhichMap[e.code];
+
+			this.key[key] = false;
+			this.keyReleased[key] = true;
 
 			this.currentKey = 0;
 		};

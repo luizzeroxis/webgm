021fc457837596f576dcea75a16fb43949fe7724
Luiz <eldorado.luiz@gmail.com>
2023-05-06 21:56:58 -0300

    Massively improved window resizing.

--- src/editor/HWindow.js
+++ src/editor/HWindow.js
@@ -1,5 +1,70 @@
 import {parent, endparent, add, HElement, HButton} from "../common/H.js";
 
+class HWindowBorder extends HElement {
+	constructor(win, side) {
+		super("div", {class: "border "+side});
+
+		this.setEvent("mousedown", e => {
+			if (e.button != 0) return;
+
+			document.removeEventListener("mousemove", this.mouseMoveHandler);
+			document.removeEventListener("mouseup", this.mouseUpHandler);
+
+			const rect = win.html.getBoundingClientRect();
+			this.offX = rect.width - (e.clientX - rect.left);
+			this.offY = rect.height - (e.clientY - rect.top);
+
+			this.offLeft = e.clientX - rect.left;
+			this.offTop = e.clientY - rect.top;
+
+			this.mouseMoveHandler = e => {
+				let x = win.x;
+				let y = win.y;
+				let w = win.w;
+				let h = win.h;
+
+				const rect = win.html.getBoundingClientRect();
+
+				const windowsAreaRect = win.editor.windowsArea.html.getBoundingClientRect();
+				const windowsAreaStyle = window.getComputedStyle(win.editor.windowsArea.html);
+
+				// right
+				if (side == "bottom-right" || side == "right" || side == "top-right") {
+					w = e.clientX + this.offX - rect.left;
+				}
+				// bottom
+				if (side == "bottom-left" || side == "bottom" || side == "bottom-right") {
+					h = e.clientY + this.offY - rect.top;
+				}
+				// left
+				if (side == "bottom-left" || side == "left" || side == "top-left") {
+					x = e.clientX - this.offLeft - (windowsAreaRect.left + parseFloat(windowsAreaStyle.borderLeftWidth)) + win.editor.windowsArea.html.scrollLeft;
+					if (x < 0) { x = 0; }
+					w = win.w + (win.x - x);
+				}
+				// top
+				if (side == "top-left" || side == "top" || side == "top-right") {
+					y = e.clientY - this.offTop - (windowsAreaRect.top + parseFloat(windowsAreaStyle.borderTopWidth)) + win.editor.windowsArea.html.scrollTop;
+					if (y < 0) { y = 0; }
+					h = win.h + (win.y - y);
+				}
+
+				win.setSize(w, h);
+				win.setPosition(x, y);
+			};
+
+			document.addEventListener("mousemove", this.mouseMoveHandler);
+
+			this.mouseUpHandler = () => {
+				document.removeEventListener("mousemove", this.mouseMoveHandler);
+				document.removeEventListener("mouseup", this.mouseUpHandler);
+			};
+
+			document.addEventListener("mouseup", this.mouseUpHandler);
+		});
+	}
+}
+
 export default class HWindow extends HElement {
 	constructor(editor, id) {
 		super("div", {class: "window"});
@@ -9,6 +74,11 @@ export default class HWindow extends HElement {
 
 		this.x = 0;
 		this.y = 0;
+		this.w = 0;
+		this.h = 0;
+
+		this.restore = {x: 0, y: 0, w: 0, h: 0};
+		this.isMaximized = false;
 
 		this.html.addEventListener("focusin", () => {
 			this.editor.windowsArea.focus(this.id);
@@ -19,52 +89,65 @@ export default class HWindow extends HElement {
 
 		parent(this);
 
-			this.titlebar = parent( add( new HElement("div", {class: "titlebar"}) ) );
+			add( new HWindowBorder(this, "top-left") );
+			add( new HWindowBorder(this, "top") );
+			add( new HWindowBorder(this, "top-right") );
+			add( new HWindowBorder(this, "left") );
 
-			this.titlebar.html.addEventListener("mousedown", e => {
-				if (e.button != 0) return;
-				if (e.target != this.html && e.target != this.title.html) return;
+			parent( add( new HElement("div", {class: "contents"}) ) );
 
-				// this.html.style.position = "absolute";
+				this.titlebar = parent( add( new HElement("div", {class: "titlebar"}) ) );
 
-				// Pos relative to window
-				const rect = this.html.getBoundingClientRect();
-				this.offX = e.clientX - rect.left;
-				this.offY = e.clientY - rect.top;
+				this.titlebar.html.addEventListener("mousedown", e => {
+					if (e.button != 0) return;
+					if (e.target != this.titlebar.html && e.target != this.title.html) return;
+					if (this.isMaximized) return;
 
-				document.removeEventListener("mousemove", this.mouseMoveHandler);
-				document.removeEventListener("mouseup", this.mouseUpHandler);
+					// Pos relative to window
+					const rect = this.html.getBoundingClientRect();
+					this.offX = e.clientX - rect.left;
+					this.offY = e.clientY - rect.top;
 
-				this.mouseMoveHandler = e => {
-					// Pos relative to windowarea
-					const rect = editor.windowsArea.html.getBoundingClientRect();
-					const style = window.getComputedStyle(editor.windowsArea.html);
-
-					let x = e.clientX - rect.left - parseFloat(style.borderLeftWidth) + editor.windowsArea.html.scrollLeft;
-					let y = e.clientY - rect.top - parseFloat(style.borderTopWidth) + editor.windowsArea.html.scrollTop;
-
-					if (x < 0) { x = 0; }
-					if (y < 0) { y = 0; }
-
-					this.setPosition(x - this.offX, y - this.offY);
-				};
-
-				document.addEventListener("mousemove", this.mouseMoveHandler);
-
-				this.mouseUpHandler = () => {
 					document.removeEventListener("mousemove", this.mouseMoveHandler);
 					document.removeEventListener("mouseup", this.mouseUpHandler);
-				};
 
-				document.addEventListener("mouseup", this.mouseUpHandler);
-			});
+					this.mouseMoveHandler = e => {
+						// Pos relative to windowarea
+						const rect = editor.windowsArea.html.getBoundingClientRect();
+						const style = window.getComputedStyle(editor.windowsArea.html);
 
-				this.title = add( new HElement("div", {class: "title"}) );
-				this.restoreMaximizeButton = add( new HButton("Maximize", () => this.restoreMaximize(), "restore-maximize") );
-				this.closeButton = add( new HButton("Close", () => this.close(), "close") );
+						let x = e.clientX - rect.left - parseFloat(style.borderLeftWidth) + editor.windowsArea.html.scrollLeft;
+						let y = e.clientY - rect.top - parseFloat(style.borderTopWidth) + editor.windowsArea.html.scrollTop;
+
+						if (x < 0) { x = 0; }
+						if (y < 0) { y = 0; }
+
+						this.setPosition(x - this.offX, y - this.offY);
+					};
+
+					document.addEventListener("mousemove", this.mouseMoveHandler);
+
+					this.mouseUpHandler = () => {
+						document.removeEventListener("mousemove", this.mouseMoveHandler);
+						document.removeEventListener("mouseup", this.mouseUpHandler);
+					};
+
+					document.addEventListener("mouseup", this.mouseUpHandler);
+				});
+
+					this.title = add( new HElement("div", {class: "title"}) );
+					this.restoreMaximizeButton = add( new HButton("Maximize", () => this.restoreMaximize(), "restore-maximize") );
+					this.closeButton = add( new HButton("Close", () => this.close(), "close") );
+					endparent();
+
+				this.client = add( new HElement("div", {class: "client"}) );
+
 				endparent();
 
-			this.client = add( new HElement("div", {class: "client"}) );
+			add( new HWindowBorder(this, "right") );
+			add( new HWindowBorder(this, "bottom-left") );
+			add( new HWindowBorder(this, "bottom") );
+			add( new HWindowBorder(this, "bottom-right") );
 
 			endparent();
 
@@ -94,20 +177,9 @@ export default class HWindow extends HElement {
 
 	getMaxSize() {
 		const windowAreaStyle = window.getComputedStyle(this.editor.windowsArea.html);
-		const windowStyle = window.getComputedStyle(this.html);
-		const titleBarStyle = window.getComputedStyle(this.titlebar.html);
-		const style = window.getComputedStyle(this.client.html);
 
-		let w = parseFloat(windowAreaStyle.width);
-		let h = parseFloat(windowAreaStyle.height);
-
-		w = w - parseFloat(windowStyle.borderLeftWidth) - parseFloat(windowStyle.borderRightWidth);
-		h = h - parseFloat(windowStyle.borderTopWidth) - parseFloat(windowStyle.borderBottomWidth);
-
-		h -= parseFloat(titleBarStyle.height);
-
-		w = w - parseFloat(style.paddingLeft) - parseFloat(style.paddingRight);
-		h = h - parseFloat(style.paddingTop) - parseFloat(style.paddingBottom);
+		const w = parseFloat(windowAreaStyle.width);
+		const h = parseFloat(windowAreaStyle.height);
 
 		return {w, h};
 	}
@@ -121,8 +193,11 @@ export default class HWindow extends HElement {
 	}
 
 	setSize(w, h) {
-		this.client.html.style.width = w.toString() + "px";
-		this.client.html.style.height = h.toString() + "px";
+		this.w = w;
+		this.h = h;
+
+		this.html.style.width = w.toString() + "px";
+		this.html.style.height = h.toString() + "px";
 	}
 
 	setPositionToDefault() {
@@ -144,10 +219,10 @@ export default class HWindow extends HElement {
 	}
 
 	setSizeToDefault(hasMaxSize=true) {
-		this.client.html.style.removeProperty("width");
-		this.client.html.style.removeProperty("height");
+		this.html.style.removeProperty("width");
+		this.html.style.removeProperty("height");
 
-		const style = window.getComputedStyle(this.client.html);
+		const style = window.getComputedStyle(this.html);
 
 		let w = parseFloat(style.width);
 		let h = parseFloat(style.height);
@@ -162,9 +237,26 @@ export default class HWindow extends HElement {
 	}
 
 	restoreMaximize() {
-		const {w, h} = this.getMaxSize();
-		this.setSize(w, h);
-		this.setPosition(0, 0);
+		if (!this.isMaximized) {
+			this.html.classList.add("maximized");
+
+			this.restore = {x: this.x, y: this.y, w: this.w, h: this.h};
+
+			const {w, h} = this.getMaxSize();
+			this.setSize(w, h);
+			this.setPosition(0, 0);
+
+			this.restoreMaximizeButton.html.textContent = "Restore";
+			this.isMaximized = true;
+		} else {
+			this.html.classList.remove("maximized");
+
+			this.setSize(this.restore.w, this.restore.h);
+			this.setPosition(this.restore.x, this.restore.y);
+
+			this.restoreMaximizeButton.html.textContent = "Maximize";
+			this.isMaximized = false;
+		}
 	}
 
 	close() {
